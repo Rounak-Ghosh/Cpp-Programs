e0b8fa023bfd2bf9ff66771cebf6a66e83dfff57
Rounak Ghosh <rounakghosh189@gmail.com>
2021-10-01 00:24:38 +0530

    Linked List

--- largestword.cpp
+++ largestword.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
-#include<math.h>
 using namespace std;
 
 int main()
 {
     int n;
-    cin >> n;   // No of words
+    cin >> n;
 
     char arr[n+1];
 
--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,31 +1,113 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class node
-{
-    public:
-    int data;
-    node* next;
 
-    node(int val)   // parameterized constructor
+
+//      METHOD 1
+
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    node(int val)
     {
         data = val;
         next = NULL;
     }
 };
 
-void insertAtTail(node* &head, int val)
+void printList(Node* n)
 {
-    node* n = new node(val);
-
-    node* temp = head;
-    while(temp -> next != NULL){
-        
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
     }
 }
 
 int main()
 {
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
 
+    head -> data = 10;
+    head -> next = second;
+    second -> data = 20;
+    second -> next = third;
+    third -> data = 30;
+    third -> next = NULL;
+ 
+    printList(head);
+ 
+    return 0;
+}
+*/
+
+
+//      METHOD 2
+
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void printList(Node* n)
+{
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+void insertAtFront(Node* &head, int val)
+{
+    Node* n = new Node();
+    n -> data = val;
+    n -> next = head;
+    head = n;
+}
+
+void insertAtEnd(Node* &head, int val)
+{
+    Node* n = new Node();
+    n -> data = val;
+    if(head == NULL)
+    {
+        head = n;
+        return;
+    }    
+    Node* last = head;
+    while(last -> next != NULL)
+        last = last->next;
+    last -> next = n;
+}
+
+int main()
+{
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head -> data = 10;
+    head -> next = second;
+    second -> data = 20;
+    second -> next = third;
+    third -> data = 30;
+    third -> next = NULL;
+ 
+    insertAtFront(head, 50);
+    insertAtEnd(head, 60);
+    printList(head);
+ 
     return 0;
 }
