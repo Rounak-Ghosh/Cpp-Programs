5bdb6ef5cf80d0638b98d0f8c0d4b7c8a9211d41
Rounak Ghosh <rounakghosh189@gmail.com>
2021-10-17 18:30:22 +0530

    Snackdown 21

---
+++ codechef_maxdistkt.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+#define ll long long
+
+bool compare(pair<ll,ll> a, pair<ll,ll> b)
+{
+    return a.second < b.second;
+}
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n, x;  
+        cin >> n;
+        vector <pair<ll,ll>> arr;
+        for(int i=0; i<n; i++)
+        {
+            cin >> x;
+            arr.push_back(make_pair(x,i));
+        }
+    
+        ll k=0;
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i].first > k)
+            {
+                arr[i].first = k;
+                k++;
+            }
+            else if(arr[i].first == k)
+            {
+                arr[i].first = k;
+            }
+        }
+        sort(arr.begin(), arr.end(), compare);
+
+        for(int i=0; i<n; i++)
+        {
+            cout << arr[i].first << " ";
+        }
+        cout << "\n" ;
+    }
+    return 0;
+}
